757994dba32c84173bff611b7fc794fa93c55ba2
TAL <truonglybach98@gmail.com>
2018-09-14 19:19:34 -0700

    Homework 7: Complete

---
+++ Homework_7/config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "UfWEhiXDgDtaysu6ahhlsylbP"
+consumer_secret = "1DPGdj1BlaZt8B7OTD62OsJIoCivvoiCHpgAGfPsm93JfgfUG9"
+access_token = "1031761564645216258-uJq4dczP7bXhyCpUkqCxi51NFQNXkX"
+access_token_secret = "ql9C4BIEO6N0A1oBgx93Et0N8cz38FfHlFTRk1L5I8jf2"
